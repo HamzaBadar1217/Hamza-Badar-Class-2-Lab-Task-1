a6b8b33a20a5496e2a3ea8b6e2e9eaa5f4946f37
HamzaMalik1601 <hamza.malik1061@gmail.com>
2022-11-03 17:36:48 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+// 1) Print first 7 elements
+console.log('Print first 7 elements: ', arr);
+
+// 2) Print Length of Array
+console.log("Length of Array: ", arr.length);
+
+// 3) Print Last Element of an array using Array Length
+console.log("Last Element of an array: ", arr[arr.length-1]);
+
+// 4) Print Second Last Element of an array using Array Length
+console.log("Second Last Element of an array: ", arr[arr.length-2]);
+
+// 4) Print Middle Element of an array using Array Length
+console.log("Middle Element of an array: ", arr[parseInt(arr.length/2)]);
